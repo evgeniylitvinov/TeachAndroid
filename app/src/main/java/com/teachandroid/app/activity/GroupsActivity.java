d65ad992fd1ca5d6731c50cdce395f303c04ccb3
rakovpublic <rakovpublic@gmail.com>
2015-03-03 18:37:49 +0200

    Fixed group activity

--- app/src/main/java/com/teachandroid/app/activity/GroupsActivity.java
+++ app/src/main/java/com/teachandroid/app/activity/GroupsActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -17,18 +18,41 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageSize;
 import com.nostra13.universalimageloader.utils.StorageUtils;
 import com.teachandroid.app.R;
+import com.teachandroid.app.api.ApiFacade;
+import com.teachandroid.app.api.SimpleResponseListener;
 import com.teachandroid.app.data.Audio;
 import com.teachandroid.app.data.Group;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GroupsActivity extends Activity {
+    private GroupAdapter groupAdapter;
 
+    private ListView groupList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
+        groupAdapter = new GroupAdapter(this, new ArrayList<Group>());
+
+        groupList = (ListView) findViewById(R.id.groups_list);
+
+        groupList.setAdapter(groupAdapter);
+
+        ApiFacade facade = new ApiFacade(this);
+        facade.getGroups(new SimpleResponseListener<List<Group>>(){
+            @Override
+            public void onResponse(final List<Group> response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        groupAdapter.addAll(response);
+                    }
+                });
+            }
+        });
     }
     private final class GroupAdapter extends ArrayAdapter<Group> {
 

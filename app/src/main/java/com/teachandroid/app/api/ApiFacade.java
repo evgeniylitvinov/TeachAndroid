7d50e1e950f8b8362b5682571136307302b31284
igor9052 <igor905.2@gmail.com>
2015-03-04 10:20:33 +0200

    Some bugs were fixed (Some bugs appeared after merging).

--- app/src/main/java/com/teachandroid/app/api/ApiFacade.java
+++ app/src/main/java/com/teachandroid/app/api/ApiFacade.java
@@ -1,20 +1,33 @@
 package com.teachandroid.app.api;
 
 import android.content.Context;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.teachandroid.app.api.reponse.*;
+import com.teachandroid.app.api.reponse.ApiResponse;
 import com.teachandroid.app.api.reponse.Error;
+import com.teachandroid.app.api.reponse.ResponseList;
 import com.teachandroid.app.data.Audio;
+import com.teachandroid.app.data.Friend;
+import com.teachandroid.app.data.Group;
 import com.teachandroid.app.data.Session;
 import com.teachandroid.app.store.SessionStore;
 import com.teachandroid.app.util.Logger;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -41,7 +54,7 @@ public class ApiFacade {
         this.httpClient = HttpClientFactory.getThreadSafeClient();
     }
 
-    public void getAudio(final ResponseListener<List<Audio>> listener){
+    public void getAudio(final ResponseListener<List<Audio>> listener) {
         RequestBuilder builder = new VkRequestBuilder("audio.get", accessToken);
         builder.addParam("count", "100");
 
@@ -59,22 +72,23 @@ public class ApiFacade {
 
                     ApiResponse<ResponseList<Audio>> apiResponse = new Gson().fromJson(reader, new TypeToken<ApiResponse<ResponseList<Audio>>>() {
                     }.getType());
-                    if(apiResponse != null){
+                    if (apiResponse != null) {
                         listener.onResponse(apiResponse.getResult().getItems());
                         listener.onError(apiResponse.getError());
-                    }else {
+                    } else {
                         listener.onError(new Error());
                     }
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
     }
-    public void getGroups(final ResponseListener<List<Group>> listener){
+
+    public void getGroups(final ResponseListener<List<Group>> listener) {
         RequestBuilder builder = new VkRequestBuilder("groups.get", accessToken);
         builder.addParam("count", "100");
-        builder.addParam("extended","1");
+        builder.addParam("extended", "1");
 
         String query = builder.query();
         Logger.log(TAG, "api request - %s", query);
@@ -88,17 +102,17 @@ public class ApiFacade {
 
                     //BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
                     String responseString = EntityUtils.toString(response.getEntity());
-                    Logger.log(TAG,"json groups request " + responseString);
+                    Logger.log(TAG, "json groups request " + responseString);
 
                     ApiResponse<ResponseList<Group>> apiResponse = new Gson().fromJson(responseString, new TypeToken<ApiResponse<ResponseList<Group>>>() {
                     }.getType());
-                    if(apiResponse != null){
+                    if (apiResponse != null) {
                         listener.onResponse(apiResponse.getResult().getItems());
                         listener.onError(apiResponse.getError());
-                    }else {
+                    } else {
                         listener.onError(new Error());
                     }
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -139,8 +153,7 @@ public class ApiFacade {
     }
 
 
-
-    public void searchAudio(final String audioKeyWord, final ResponseListener<List<Audio>> listener){
+    public void searchAudio(final String audioKeyWord, final ResponseListener<List<Audio>> listener) {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -152,7 +165,7 @@ public class ApiFacade {
                     urlBuilder.append("count").append("=").append(URLEncoder.encode("100", "UTF-8")).append("&");
                     urlBuilder.append("v").append("=").append(URLEncoder.encode("5.28", "UTF-8")).append("&");
                     urlBuilder.append("access_token").append("=").append(URLEncoder.encode(accessToken, "UTF-8"));
-                }catch (UnsupportedEncodingException e){
+                } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
                 HttpURLConnection connection = null;
@@ -167,7 +180,7 @@ public class ApiFacade {
 
                     BufferedReader reader = new BufferedReader(new InputStreamReader(in));
                     String response = null;
-                    while ((response = reader.readLine()) != null){
+                    while ((response = reader.readLine()) != null) {
                         Logger.log(TAG, "url response " + response);
                     }
 
@@ -176,10 +189,10 @@ public class ApiFacade {
                     }.getType();
 
                     ApiResponse<ResponseList<Audio>> apiResponse = gson.fromJson(response, type);
-                    if(apiResponse != null){
+                    if (apiResponse != null) {
                         listener.onResponse(apiResponse.getResult().getItems());
                         listener.onError(apiResponse.getError());
-                    }else {
+                    } else {
                         listener.onError(new Error());
                     }
 
